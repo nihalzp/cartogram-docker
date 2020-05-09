6e83d876957785ccd7f118195a8642b30c30f406
Ian Duncan <janskykd@gmail.com>
2020-05-09 12:01:34 +0800

    updated shell scripts

--- addmap.sh
+++ addmap.sh
@@ -1,25 +1,7 @@
 #!/bin/bash
 
-if [ `uname -s` = Linux ]
-then
-	CONTAINER=`sudo docker-compose ps | awk '$1~/web/ && $0~/Up/ { print $1 }'`
-	if [ -z "$CONTAINER" ]
-	then
-		echo "Docker application not started."
-		exit 1
-	else
-		sudo docker exec -it $CONTAINER python addmap.py "$@"
-		sudo chown `id -un`:`id -gn` -R cartogram-web/
-	fi
-else
-	CONTAINER=`docker-compose ps | awk '$1~/web/ && $0~/Up/ { print $1 }'`
-        if [ -z "$CONTAINER" ]
-        then
-                echo "Docker application not started."
-                exit 1
-        else
-                docker exec -it $CONTAINER python addmap.py "$@"
-        fi
-fi
+./runcmd.sh web python addmap.py "$@"
+
+
 
 

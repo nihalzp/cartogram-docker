ade796cbdaee3ddb26b96f630e6cabc922d16866
Ian Duncan <janskykd@gmail.com>
2020-05-13 00:32:24 +0800

    remove sudo from runcmd for Linux

--- runcmd.sh
+++ runcmd.sh
@@ -1,24 +1,11 @@
 #!/bin/bash
 
-if [ `uname -s` = Linux ]
+
+CONTAINER=`docker-compose ps | awk -v container="$1" '$1~container && $0~/Up/ { print $1 }'`
+if [ -z "$CONTAINER" ]
 then
-	CONTAINER=`sudo docker-compose ps | awk -v container="$1" '$1~container && $0~/Up/ { print $1 }'`
-	if [ -z "$CONTAINER" ]
-	then
-		echo "Docker application not started."
-		exit 1
-	else
-		sudo docker exec -it $CONTAINER "${@:2}"
-		sudo chown `id -un`:`id -gn` -R cartogram-web/
-		sudo chown `id -un`:`id -gn` -R cartogram-serverless/
-	fi
+	echo "Docker application not started."
+	exit 1
 else
-	CONTAINER=`docker-compose ps | awk -v container="$1" '$1~container && $0~/Up/ { print $1 }'`
-        if [ -z "$CONTAINER" ]
-        then
-                echo "Docker application not started."
-                exit 1
-        else
-                docker exec -it $CONTAINER "${@:2}"
-        fi
+	docker exec -it $CONTAINER "${@:2}"
 fi
